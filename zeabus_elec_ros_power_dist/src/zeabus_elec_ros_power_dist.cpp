cdf42191da0f0202dc43921a01deb08cfca3d0c1
psychoAB <natchanan.th@outlook.com>
2019-04-10 00:25:08 +0700

    made zeabus_elec_ros_power_dist into ros2

--- zeabus_elec_ros_power_dist/src/zeabus_elec_ros_power_dist.cpp
+++ zeabus_elec_ros_power_dist/src/zeabus_elec_ros_power_dist.cpp
@@ -1,123 +1,123 @@
-#include <ros/ros.h>
-#include <std_msgs/String.h>
-#include <zeabus_elec_ros_power_dist/power_dist.h>
-#include <ftdi_impl.h>
-
+#include <cstdint>
+#include <cstdio>
+#include <string>
 #include <memory>
-#include <sstream>
+
+#include "rclcpp/rclcpp.hpp"
+#include "zeabus_elec_ros_power_dist/srv/power_dist.hpp"
+
+#include "ftdi_impl.h"
 
 /* ===================================================
- * File-scope global variables
+ * ROS node defination
  * ===================================================
  */
-static const char* cstPowerDistSerial = "PowerDist";
-
-static std::shared_ptr<Zeabus_Elec::ftdi_mpsse_impl> pxMssp;
-static ros::Publisher errMsgPublisher;	/* Publisher for error message */
-static ros::ServiceServer switchServiceServer; /* Service server for switch-controlling */
-
-/* ===================================================
- * ROS service subroutines
- * ===================================================
- */
-bool ZeabusElec_SetSwitch( zeabus_elec_ros_power_dist::power_dist::Request &req,
-                            zeabus_elec_ros_power_dist::power_dist::Response &res )
+namespace
 {
-	int ftStat;
-        uint8_t switchState, currentSwitchState, switchMask;
+    class PowerDistNode : public rclcpp::Node
+    {
+        public:
+            explicit PowerDistNode( const std::string &kx_node_name, const std::string &kx_service_name );
 
-        res.result = true;
+            std::shared_ptr<Zeabus_Elec::ftdi_mpsse_impl> px_mssp_;
 
-        switchMask = 0x01 << ( req.switchIndex );
+        private:
+            rclcpp::Service<zeabus_elec_ros_power_dist::srv::PowerDist>::SharedPtr px_srv_;
+    };
+}
 
-        currentSwitchState = pxMssp->ReadHiGPIOData();
+PowerDistNode::PowerDistNode( const std::string &kx_node_name, const std::string &kx_service_name ) : rclcpp::Node( kx_node_name )
+{
+    auto v_handle_elec_power_dist = [this]( const std::shared_ptr<zeabus_elec_ros_power_dist::srv::PowerDist::Request> kx_req,
+                                            std::shared_ptr<zeabus_elec_ros_power_dist::srv::PowerDist::Response> x_res ) -> void
+    {
+        int ft_state;
+        uint8_t u_switch_state, u_current_switch_state, u_switch_mask;
 
-        if( req.isSwitchHigh )
+        x_res->b_result = true;
+        u_switch_mask = 0x01U << ( kx_req->u_switch_index );
+        u_current_switch_state = this->px_mssp_->ReadHiGPIOData();
+
+        if( kx_req->is_switch_high )
         {
-            switchState = ( currentSwitchState | switchMask );
+            u_switch_state = ( u_current_switch_state | u_switch_mask );
         }
         else
         {
-            switchState = ( currentSwitchState & ~( switchMask ) );
+            u_switch_state = ( u_current_switch_state & ~( u_switch_mask ) );
         }
-	
-	ftStat = pxMssp->SetHiGPIOData( switchState );
-	if( ftStat != 0 )
-	{
-		/* Some Error occurred. So, we publish the error message */
-		std_msgs::String msg;
-		std::stringstream ss;
-		ss << "Error in power-distributor controller with the code " << ftStat;
-		msg.data = ss.str();
-		
-		errMsgPublisher.publish( msg );
 
-                res.result = false;
-	}
+        ft_state = this->px_mssp_->SetHiGPIOData( u_switch_state );
 
-        return res.result;
+        if( ft_state != 0 )
+        {
+            RCLCPP_ERROR( this->get_logger(), "Unable to set power switch of power_distributor" );
+            x_res->b_result = false;
+        }
+
+        return;
+    };
+
+    /* Register ROS service server to Elec/Power_switch topic */
+    this->px_srv_ = this->create_service<zeabus_elec_ros_power_dist::srv::PowerDist> ( kx_service_name, v_handle_elec_power_dist );
+    
+    return;
 }
 
 /* ===================================================
  * main
  * ===================================================
  */
-int main( int argc, char** argv )
+int main( int argc, char *argv[] )
 {
-        int paramInitIODirection, paramInitIOPinState;
-        uint16_t initIODirection, initIOPinState;
+        setvbuf( stdout, NULL, _IONBF, BUFSIZ );
 
 	/* Initialize ROS functionalities */	
-	ros::init(argc, argv, "Zeabus_Elec_Power_dist");
- 	ros::NodeHandle nh("/zeabus/elec");
+        rclcpp::init( argc, argv );
 
-        /* Retrieve parameter from launch file */
-        nh.param < int > ("/Zeabus_Elec_Power_dist/IODirection", paramInitIODirection, 0xFFFF);
-        nh.param < int > ("/Zeabus_Elec_Power_dist/IOPinState", paramInitIOPinState, 0x0000);
+        static const std::string kac_POWER_DISTRIBUTOR_DESCRIPTION = std::string( "PowerDist" );
+        static const std::string kx_POWER_DISTRIBUTOR_NODE_NAME = std::string( "PowerDist" );
+        static const std::string kx_SET_POWER_SWITCH_SERVICE_NAME = std::string( "elec/power_dist/set_power_swtich" );
 
-        /* cast int to uint16_t because NodeHandle::param doesn't support uint16_t */
-        initIODirection = static_cast<uint16_t>(paramInitIODirection);
-        initIOPinState = static_cast<uint16_t>(paramInitIOPinState);
+        static std::shared_ptr<PowerDistNode> px_node = std::make_shared<PowerDistNode>( kx_POWER_DISTRIBUTOR_NODE_NAME, kx_SET_POWER_SWITCH_SERVICE_NAME );
+
+        // TODO: Retrieve parameter from launch file
 
 	/*=================================================================================
 	  Discover the Power Distributor and also open handles for it.
 	  =================================================================================*/
 
 	/* Create the device manager class to implement chip functions */
-	pxMssp = std::make_shared<Zeabus_Elec::ftdi_mpsse_impl>( Zeabus_Elec::FT232H, cstPowerDistSerial );
+        px_node->px_mssp_ = std::make_shared<Zeabus_Elec::ftdi_mpsse_impl>( Zeabus_Elec::FT232H, kac_POWER_DISTRIBUTOR_DESCRIPTION.c_str() );
 	
-	if( pxMssp->GetCurrentStatus() != 0 )
+        if( px_node->px_mssp_->GetCurrentStatus() != 0U )
 	{
 		/* Fail - unable to initialize Power Distribution module */
-		ROS_FATAL( "Unable to initialize Power Distribution module" );
-		return( -1 );
-		
+                RCLCPP_FATAL( px_node->get_logger(), "Unable to initialize power_distributor" );
+                // TODO: Proper exception handling
+		return( -1 );       // Let's pretend this return statement is throwing an exception
 	}
 	
 	/* Set GPIO direction and pin intial state to all output, bit=1 means output, 0 means input */
-	pxMssp->SetGPIODirection( initIODirection , initIOPinState );	/* All bits are output, initial pin state is low */
-	if( pxMssp->GetCurrentStatus() != 0 )
+        px_node->px_mssp_->SetGPIODirection( 0xFFFFU , 0x0000U );       /* All bits are output, initial pin state is low */
+        if( px_node->px_mssp_->GetCurrentStatus() != 0U )
 	{
 		/* Fail - unable to initialize Power Distribution module */
-		ROS_FATAL( "Unable to initialize set-up GPIO direction" );
-		return( -1 );
-		
+                RCLCPP_FATAL( px_node->get_logger(), "Unable to setup GPIO direction of power_distributor" );
+                // TODO: Proper exception handling
+		return( -1 );       // Let's pretend this return statement is throwing an exception
 	}
 	
 	/*=================================================================================
 	  Now the FTDI chip is opened and hooked. We can continue ROS registration process 
 	  =================================================================================*/
 	
-	/* Register ROS publisher to Elec/Hw_error topic */
-	errMsgPublisher = nh.advertise<std_msgs::String>( "hw_error", 1000 );
-	/* Register ROS service server to Elec/Power_switch topic */
-	switchServiceServer = nh.advertiseService( "power_switch", ZeabusElec_SetSwitch );
-
 	/* Main-loop. Just a spin-lock */
-	ros::spin();
+        rclcpp::spin( px_node );
 	
 	/*=================================================================================
 	  At this point of code. ROS has some fatal errors or just normal shutdown. Also us. 
 	  =================================================================================*/
-	return( 0 );
+        rclcpp::shutdown();
+        return 0;
 }
