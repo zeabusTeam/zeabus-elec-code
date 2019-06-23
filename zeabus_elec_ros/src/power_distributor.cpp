961830ab8af3c6430f33fc0ae30606a5dab0d8c8
psychoAB <natchanan.th@outlook.com>
2019-06-24 02:46:40 +0700

    fixed power_dist to power_distributor in zeabus_elec_ros

--- zeabus_elec_ros/src/power_distributor.cpp
+++ zeabus_elec_ros/src/power_distributor.cpp
@@ -40,7 +40,7 @@ static const std::string kx_POWER_DISTRIBUTOR_DESCRIPTION = "PowerDist";
 static const uint16_t kus_INITIAL_IO_DIRECTION =    0xFFFFU;
 static const uint16_t kus_INITIAL_IO_PIN_STATE =    0x0000U;
 
-static std::shared_ptr<Zeabus_Elec::ftdi_mpsse_impl> px_power_dist;
+static std::shared_ptr<Zeabus_Elec::ftdi_mpsse_impl> px_power_distributor;
 
 static ros::Publisher x_publisher_node_status_log;
 static ros::Publisher x_publisher_hardware_error_log;
@@ -52,7 +52,7 @@ static bool b_set_power_swtich( zeabus_elec_ros::ServicePowerSwitch::Request &x_
                                 zeabus_elec_ros::ServicePowerSwitch::Response &x_response )
 {
     bool b_return = true;
-    int i_status_power_dist;
+    int i_status_power_distributor;
     uint8_t u_switch_state, u_swtich_state_current, u_switch_mask;
     std::string x_description;
 
@@ -74,7 +74,7 @@ static bool b_set_power_swtich( zeabus_elec_ros::ServicePowerSwitch::Request &x_
     u_switch_mask = 1U << ( x_request.u_switch_index );
 
     // get current GPIO pin state
-    u_swtich_state_current = px_power_dist->ReadHiGPIOData();
+    u_swtich_state_current = px_power_distributor->ReadHiGPIOData();
 
     if( x_request.is_switch_high )
     {
@@ -88,9 +88,9 @@ static bool b_set_power_swtich( zeabus_elec_ros::ServicePowerSwitch::Request &x_
     }
     
     // set new GPIO pin state
-    i_status_power_dist = px_power_dist->SetHiGPIOData( u_switch_state );
+    i_status_power_distributor = px_power_distributor->SetHiGPIOData( u_switch_state );
 
-    if( i_status_power_dist != 0U )
+    if( i_status_power_distributor != 0U )
     {
         // unable to set GPIO pin state of power distributor
         b_return = false;
@@ -98,7 +98,7 @@ static bool b_set_power_swtich( zeabus_elec_ros::ServicePowerSwitch::Request &x_
         // print and publish unable to set GPIO pin state of power distributor log
         v_log_hardware_error(   x_publisher_hardware_error_log,
                                 ( int64_t )ki_ERROR_UNABLE_TO_SET_POWER_DISTRIBUTOR_GPIO_PIN_STATE,
-                                ( int64_t )i_status_power_dist,
+                                ( int64_t )i_status_power_distributor,
                                 std::string( "Unable to set GPIO pin state of power distributor" ) );
     }
 
@@ -127,12 +127,12 @@ int main( int argc, char **argv )
     i_main_status = ki_ERROR_NONE;
 
     // init ROS node
-    ros::init( argc, argv, "zeabus_elec_power_dist" );
+    ros::init( argc, argv, "zeabus_elec_power_distributor" );
     ros::NodeHandle x_node_handle( "/elec" );
 
     // retrive parameters from the launch file
-    x_node_handle.param<int>( "/zeabus_elec_power_dist/i_initial_io_direction", i_param_initial_io_direction, kus_INITIAL_IO_DIRECTION );
-    x_node_handle.param<int>( "/zeabus_elec_power_dist/i_initial_io_pin_state", i_param_initial_io_pin_state, kus_INITIAL_IO_PIN_STATE );
+    x_node_handle.param<int>( "/zeabus_elec_power_distributor/i_initial_io_direction", i_param_initial_io_direction, kus_INITIAL_IO_DIRECTION );
+    x_node_handle.param<int>( "/zeabus_elec_power_distributor/i_initial_io_pin_state", i_param_initial_io_pin_state, kus_INITIAL_IO_PIN_STATE );
 
     // cast int to uint16_t because NodeHandle::param doesn't support uint16_t
     us_initial_io_direction = ( uint16_t )i_param_initial_io_direction;
@@ -152,20 +152,20 @@ int main( int argc, char **argv )
     v_log_node_status( x_publisher_node_status_log, std::string( "Power distributor node started" ) );
 
     // create the device manager object, also open the device
-    px_power_dist = std::make_shared<Zeabus_Elec::ftdi_mpsse_impl>( Zeabus_Elec::FT232H, kx_POWER_DISTRIBUTOR_DESCRIPTION );
+    px_power_distributor = std::make_shared<Zeabus_Elec::ftdi_mpsse_impl>( Zeabus_Elec::FT232H, kx_POWER_DISTRIBUTOR_DESCRIPTION );
     
     try
     {
         // check the device manager object status after created
-        if( px_power_dist->GetCurrentStatus() != 0U )
+        if( px_power_distributor->GetCurrentStatus() != 0U )
         {
             // unable to init power distributor
             throw( ki_ERROR_UNABLE_TO_OPEN_POWER_DISTRIBUTOR );
         }
         
         // set initialize GPIO direction and GPIO pin state to all output
-        px_power_dist->SetGPIODirection( us_initial_io_direction , us_initial_io_pin_state );
-        if( px_power_dist->GetCurrentStatus() != 0U )
+        px_power_distributor->SetGPIODirection( us_initial_io_direction , us_initial_io_pin_state );
+        if( px_power_distributor->GetCurrentStatus() != 0U )
         {
             // unable to initialize GPIO direction and GPIO pin state of power distributor
             throw( ki_ERROR_UNABLE_TO_INIT_POWER_DISTRIBUTOR_GPIO );
@@ -200,7 +200,7 @@ int main( int argc, char **argv )
         // print and publish the log
         v_log_hardware_error_fatal( x_publisher_hardware_error_log,
                                     ( int64_t )ki_error,
-                                    ( int64_t )px_power_dist->GetCurrentStatus(),
+                                    ( int64_t )px_power_distributor->GetCurrentStatus(),
                                     x_description );
     }
 
